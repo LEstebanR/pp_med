6121c42c894a0d321ec4e3708fc09255629c709d
Luis Esteban R <leramirezca@gmail.com>
2024-08-03 18:29:46 -0500

    fix footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -14,9 +14,9 @@ const Footer: React.FC = () => {
         </a>
         <div className="mx-2 h-[30px] w-[0.5px] rotate-[20deg] bg-black"></div>
         <div className="flex items-center gap-x-1 text-xs sm:text-base">
-          <span className="text-slate-500">Made with</span>
+          <span className="text-slate-500">Hecho con</span>
           <GoHeart className="h-4 w-4 text-red-500" />
-          <span className="text-slate-500">by</span>
+          <span className="text-slate-500">por</span>
           <a
             className="font-medium text-primary underline decoration-dashed decoration-0 underline-offset-4"
             href="https://github.com/LEstebanR"
