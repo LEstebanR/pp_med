5956129d519add704db357bf37defe4c2dacf264
Luis Esteban R <leramirezca@gmail.com>
2024-08-03 18:27:06 -0500

    fix weekends

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -10,18 +10,22 @@ const Card: FC<props> = ({ vehicle, day, date }) => {
   const getRestriction = (day: string, date: string) => {
     switch (vehicle.type) {
       case "car":
-        return `Placa terminada en ${vehicle.law[day].map(
-          (number: number) => number
-        )}`;
+        return vehicle.law[day]
+          ? `Placa terminada en ${vehicle.law[day].map(
+              (number: number) => number
+            )}`
+          : "Hoy no hay restricción";
       case "bike":
-        return `Placa inicia en ${vehicle.law[day].map(
-          (number: number) => number
-        )}`;
+        return vehicle.law[day]
+          ? `Placa inicia en ${vehicle.law[day].map(
+              (number: number) => number
+            )}`
+          : "Hoy no hay restricción";
       case "taxi":
         console.log(vehicle.law[date]);
         return vehicle.law[date] !== undefined
           ? `Placa termina en ${vehicle.law[date]}`
-          : "No hay";
+          : "Hoy no hay restricción";
     }
   };
   return (
@@ -29,7 +33,6 @@ const Card: FC<props> = ({ vehicle, day, date }) => {
       <p>{vehicle.emoji}</p>
       <p>{vehicle.copy}</p>
       <p>{getRestriction(day, date)}</p>
-      <p>{date}</p>
     </div>
   );
 };
